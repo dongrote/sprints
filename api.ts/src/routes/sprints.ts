a48a3362f085e84dbb27f620b02d2b44c0ebbe06
Don Grote <don.grote@gmail.com>
2021-01-17 16:58:40 -0500

    implement POST sprint and POST transactions

--- api.ts/src/routes/sprints.ts
+++ api.ts/src/routes/sprints.ts
@@ -1,8 +1,10 @@
 import { Request, Response, NextFunction, Router } from 'express';
+import _ from 'lodash';
 import HttpError from 'http-error-constructor';
 import BurndownChart from '../core/BurndownChart';
 import Sprint from '../core/Sprint';
 import SprintTransaction from '../core/SprintTransaction';
+import { SprintTransactionAction } from '../core/types';
 
 const router = Router();
 
@@ -14,7 +16,18 @@ router.get('/', async (req: Request, res: Response, next: NextFunction): Promise
     return next(err);
   }
 });
-router.post('/');
+router.post('/', async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    res.json(await Sprint.createInProject(req.body.ProjectId, {
+      name: req.body.name,
+      predictedPoints: req.body.points,
+      startAt: req.body.startAt,
+      endAt: req.body.endAt,
+    }));
+  } catch (err) {
+    return next(err);
+  }
+});
 router.get('/:id', async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   const SprintId = Number(req.params.id);
   try {
@@ -43,6 +56,21 @@ router.get('/:id/transactions', async (req: Request, res: Response, next: NextFu
   try {
     if (isNaN(SprintId)) throw new HttpError(400, `invalid SprintId: '${req.params.id}'`);
     res.json(await SprintTransaction.findAllInSprint(SprintId));
+  } catch (err) {
+    return next(err);
+  }
+});
+router.post('/:id/transactions', async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const timestamp = _.has(req.body, 'timestamp') ? new Date(req.body.timestamp) : new Date();
+  try {
+    const SprintId = Number(req.params.id);
+    const action: SprintTransactionAction = req.body.action;
+    const sprint = await Sprint.findById(SprintId);
+    if (sprint === null) throw new HttpError(404);
+    if (action === SprintTransactionAction.Claim) await sprint.claimStory(req.body.StoryId, {timestamp});
+    if (action === SprintTransactionAction.Remit) await sprint.remitStory(req.body.StoryId, {timestamp});
+    if (action === SprintTransactionAction.Complete) await sprint.completeStory(req.body.StoryId, {timestamp});
+    res.sendStatus(204);
   } catch (err) {
     return next(err);
   }
