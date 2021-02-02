015c2c964048e74672197e994ecc79f0dd9ebc50
Don Grote <don.grote@gmail.com>
2021-02-02 11:16:40 -0500

    Add an Average Velocity line to the Velocity Chart

--- webui/src/components/VelocityChart.js
+++ webui/src/components/VelocityChart.js
@@ -1,21 +1,25 @@
 import { Chart } from 'react-google-charts';
 
-const VelocityChart = props => (
-  <Chart
-    chartType='ColumnChart'
-    loader={<div>Loading Data ...</div>}
-    data={[['Sprint', 'Velocity']].concat(props.velocities.map((v,i) => ([`${i}`,v])))}
-    options={{
-      animation: {startup: true, duration: 500, easing: 'in'},
-      title: 'Project Velocity',
-      hAxis: {viewWindow: {min: 0}, title: 'Sprints'},
-      vAxis: {minValue: 0},
-      legend: {position: 'none'},
-      trendlines: {
-        0: {type: 'linear', color: 'purple', lineWidth: 1, opacity: 0.3}
-      },
-    }}
-  />
-);
+const VelocityChart = props => {
+  const mean = props.velocities.length ? props.velocities.reduce((acc, val) => acc + val) / props.velocities.length : 0;
+  return (
+    <Chart
+      chartType='ComboChart'
+      loader={<div>Loading Data ...</div>}
+      data={[['Sprint', 'Velocity', `Average Velocity (${mean})`]].concat(props.velocities.map((v,i) => ([`${i}`,v, mean])))}
+      options={{
+        animation: {startup: true, duration: 200, easing: 'in'},
+        title: 'Project Velocity',
+        hAxis: {viewWindow: {min: 0}, title: 'Sprints'},
+        vAxis: {minValue: 0},
+        legend: {position: 'in'},
+        series: {
+          0: { type: 'bars', color: 'blue', visibleInLegend: false },
+          1: { type: 'line', color: 'purple', lineWidth: 1, opacity: 0.3, pointsVisible: false },
+        },
+      }}
+    />
+  );
+};
 
 export default VelocityChart;
