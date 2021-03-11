382f3ab1c45bc5618a387dcdaf411f939c15ffd1
Don Grote <don.grote@gmail.com>
2021-03-11 12:38:51 -0500

    BurndownChart has two render modes
    BurndownChart defaults to render the traditional LineChart.
    Clicking on the BurndownChart changes to a breakdown of story point claims/removals by day.

--- api.ts/src/core/BurndownChart.ts
+++ api.ts/src/core/BurndownChart.ts
@@ -4,6 +4,12 @@ import _ from 'lodash';
 import dayjs from 'dayjs';
 import { SprintTransactionAction } from './types';
 
+export interface BurndownValues {
+  net: number[],
+  inc: number[],
+  dec: number[],
+}
+
 class BurndownChart {
   static async createFromSprint(SprintId: number) {
     return new BurndownChart(SprintId);
@@ -17,13 +23,15 @@ class BurndownChart {
     const dates = await sprint.sprintDays();
     return dates.map(d => dayjs(d).format('MM/DD/YYYY'));
   }
-  async realValues(): Promise<number[]> {
+  async realValues(): Promise<BurndownValues> {
     const [sprint, transactions] = await Promise.all([
       Sprint.findById(this.#SprintId), SprintTransaction.findAllInSprint(this.#SprintId)
     ]);
     const startAtDay = dayjs(sprint.startAt).startOf('d'),
       endAtDay = dayjs(sprint.endAt).startOf('d'),
-      values = [];
+      values = [],
+      increments = [],
+      decrements = [];
     for (let points = 0, dayCursor = startAtDay; dayCursor.isBefore(endAtDay, 'd') || dayCursor.isSame(endAtDay, 'd'); dayCursor = dayCursor.add(1, 'd')) {
       points = _.reduce(transactions.results, (acc, transaction): number => {
         if (dayCursor.isSame(dayjs(transaction.ts).startOf('d'), 'd')) {
@@ -34,8 +42,22 @@ class BurndownChart {
         return acc;
       }, points);
       values.push(points);
+      const incrs = _.reduce(transactions.results, (acc, transaction): number => {
+        if (dayCursor.isSame(dayjs(transaction.ts).startOf('d'), 'd')) {
+          return transaction.action === SprintTransactionAction.Claim ? acc + transaction.points : acc;
+        }
+        return acc;
+      }, 0);
+      increments.push(incrs);
+      const decrs = _.reduce(transactions.results, (acc, transaction): number => {
+        if (dayCursor.isSame(dayjs(transaction.ts).startOf('d'), 'd')) {
+          return _.includes([SprintTransactionAction.Complete, SprintTransactionAction.Remit], transaction.action) ? acc + transaction.points : acc;
+        }
+        return acc;
+      }, 0);
+      decrements.push(decrs);
     }
-    return values;
+    return {net: values, inc: increments, dec: decrements};
   }
   async idealValues(): Promise<number[]> {
     const sprint = await Sprint.findById(this.#SprintId);
--- webui/src/components/BurndownChart.js
+++ webui/src/components/BurndownChart.js
@@ -1,40 +1,109 @@
+import { Component } from 'react';
 import { Chart } from 'react-google-charts';
 import day from 'dayjs';
 
-const BurndownChart = props => {
-  const today = day().startOf('d'),
-    dailyPoints = [];
-  props.real.forEach((remaining, idx) => {
-    const dailyPointsDay = day(props.labels[idx], 'MM/DD/YYYY');
-    let ideal = null;
-    if (idx === 0) ideal = props.ideal[0];
-    if (idx === props.real.length - 1) ideal = 0;
-    dailyPoints.push({
-      label: dailyPointsDay.format('M/D'),
-      ideal,
-      remaining,
-      tooltip: dailyPointsDay.format('ddd MMM D'),
-      certainty: dailyPointsDay.isBefore(today),
-    });
-  });
-  return (
-  <Chart
-    chartType='LineChart'
-    loader={<div>Loading Data ...</div>}
-    data={[['Day', 'Ideal', 'Real', {role: 'tooltip'}, {role: 'certainty'}]].concat(dailyPoints.map(dp => ([dp.label, dp.ideal, dp.remaining, dp.tooltip, dp.certainty])))}
-    options={{
-      animation: {startup: true, duration: 100, easing: 'in'},
-      legend: {position: 'top'},
-      hAxis: {viewWindow: {min: 0}, maxAlternation: 2},
-      vAxis: {minValue: 0},
-      interpolateNulls: true,
-      series: {
-        0: {color: 'orange', lineDashStyle: [4, 1]},
-        1: {color: 'blue', pointSize: 5},
-      },
-    }}
-  />
-);
-  };
+const ChartStyles = {
+  LINE: 'line',
+  COLUMN: 'column',
+};
 
-export default BurndownChart;
+export default class BurndownChart extends Component {
+  state = { style: ChartStyles.LINE };
+
+  toggleLineView() { this.setState({style: ChartStyles.LINE}); }
+  toggleColumnView() { this.setState({style: ChartStyles.COLUMN}); }
+  toggleView() { this.state.style === ChartStyles.LINE ? this.toggleColumnView() : this.toggleLineView(); }
+
+  dailyNetPoints() {
+    const today = day().startOf('d'),
+      { net, labels, ideals } = this.props,
+      dailyPoints = [];
+    net.forEach((remaining, idx) => {
+      const dailyPointsDay = day(labels[idx], 'MM/DD/YYYY');
+      let ideal = null;
+      if (idx === 0) ideal = ideals[0];
+      if (idx === net.length - 1) ideal = 0;
+      dailyPoints.push({
+        label: dailyPointsDay.format('M/D'),
+        ideal,
+        remaining,
+        tooltip: dailyPointsDay.format('ddd MMM D'),
+        certainty: dailyPointsDay.isBefore(today),
+      });
+    });
+    return dailyPoints;
+  }
+
+  dailyAddRemovePoints() {
+    const { inc, dec, labels, ideals } = this.props,
+      dailyPoints = [];
+    inc.forEach((add, idx) => {
+      const dailyPointsDay = day(labels[idx], 'MM/DD/YYYY'),
+        remove = dec[idx];
+      let ideal = null;
+      if (idx === 0) ideal = ideals[0];
+      if (idx === inc.length - 1) ideal = 0;
+      dailyPoints.push({
+        label: dailyPointsDay.format('M/D'),
+        ideal,
+        add,
+        remove,
+        tooltip: `${dailyPointsDay.format('ddd MMM D')} (+${add}/-${remove})`,
+      });  
+    });
+    return dailyPoints;
+  }
+
+  renderColumn() {
+    return (
+      <Chart
+      chartType='ComboChart'
+      loader={<div>Loading Data ...</div>}
+      data={[['Day', 'Ideal', 'Removed', {role: 'tooltip'}, 'Claimed', {role: 'tooltip'}]].concat(this.dailyAddRemovePoints().map(v => ([v.label, v.ideal, v.remove, v.tooltip, v.add, v.tooltip])))}
+      options={{
+        animation: {startup: true, duration: 100, easing: 'in'},
+        hAxis: {viewWindow: {min: 0}, maxAlternation: 2},
+        vAxis: {minValue: 0},
+        legend: {position: 'top'},
+        isStacked: true,
+        interpolateNulls: true,
+        series: {
+          0: { type: 'line', color: 'orange', lineDashStyle: [4, 1], pointsVisible: false },
+          1: { type: 'bars', color: 'red', isStacked: true },
+          2: { type: 'bars', color: 'blue', isStacked: true },
+        },
+      }}
+      />
+    );
+  }
+
+  renderLine() {
+    return (
+      <Chart
+        onClick={() => this.toggleColumnView()}
+        chartType='LineChart'
+        loader={<div>Loading Data ...</div>}
+        data={[['Day', 'Ideal', 'Real', {role: 'tooltip'}, {role: 'certainty'}]].concat(this.dailyNetPoints().map(dp => ([dp.label, dp.ideal, dp.remaining, dp.tooltip, dp.certainty])))}
+        options={{
+          animation: {startup: true, duration: 100, easing: 'in'},
+          legend: {position: 'top'},
+          hAxis: {viewWindow: {min: 0}, maxAlternation: 2},
+          vAxis: {minValue: 0},
+          interpolateNulls: true,
+          series: {
+            0: {color: 'orange', lineDashStyle: [4, 1]},
+            1: {color: 'blue', pointSize: 5},
+          },
+        }}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div onClick={() => this.toggleView()}>
+        {this.state.style === ChartStyles.LINE ? this.renderLine() : this.renderColumn()}
+      </div>
+    );
+  }
+}
