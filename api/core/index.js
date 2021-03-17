14d40e728e5e81d78a0210a3189a904d4ca887b1
Don Grote <don.grote@gmail.com>
2021-03-16 20:05:12 -0400

    removed stale CommonJS api server

--- api/core/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const Project = require('./Project'),
-  Sprint = require('./Sprint'),
-  UserStory = require('./UserStory');
-
-exports = module.exports = {Project, Sprint, UserStory};
